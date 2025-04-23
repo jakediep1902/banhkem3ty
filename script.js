f72092a418d3be84c5f3e13854bc375f99962451
Dương Unity (Jake) <jakediep1902@gmail.com>
2025-04-23 18:08:49 +0700

    #250423 Uploaded whole project

---
+++ script.js
@@ -0,0 +1,21 @@
+function sayHello(name) {
+    console.log("Hello, ".concat(name, "!"));
+}
+sayHello("TypeScript");
+function GetSCkey()
+{
+    return "IPR43Z1OX7DYWORMIXIRZNFYDZ4GTOYNOX75N1N8UDDSCFY3WT"
+}
+function GetTitleID()
+{
+    return "CEB9E"
+}
+// Đảm bảo code chạy sau khi DOM đã tải
+document.addEventListener("DOMContentLoaded", function () {
+    const htmlContent = `
+        <h1>BÁNH KEM 3 TỶ KÍNH CHÀO QUÝ KHÁCH</h1>
+        <p>Uy tín, Chất lượng, Tận tâm</p>
+    `;
+
+    document.getElementById("content").innerHTML = htmlContent;
+});
